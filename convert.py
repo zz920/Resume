e4044b38ad1ffc0b39486ca24eb513e9153372c7
Bowen zhi <b.zhi@fetchr.us>
2019-10-26 07:03:01 -0700

    customize the css, need to fixed the zh resume

--- convert.py
+++ convert.py
@@ -7,17 +7,19 @@ import logging
 import subprocess
 import argparse
 from distutils import dir_util
-from pywebcopy import save_website
 
 
-def start_grip_server(source_file):
-    process = subprocess.Popen(['grip', source_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # wait for server start
-    time.sleep(0.5)
-    if not process.poll():
-        return process
-    return None
-
+def start_grip_server(source_file, target_folder):
+    html_file = os.path.join(target_folder, "resume.html")
+    process = subprocess.Popen(['grip', source_file, '--export', html_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process.wait(timeout=3)
+    with open(html_file, 'r') as f:
+        content = f.read()
+    with open(html_file, 'w') as f:
+        content = remove_title(content)
+        content = insert_custom_css(content)
+        f.write(content)
+    return html_file
 
 def close_grip_server(process):
     process.terminate()
@@ -27,27 +29,42 @@ def remove_title(content):
     pa = re.compile(r"<h3.*?octicon octicon-book.*?h3>", re.DOTALL)
     return re.sub(pa, "", content)
 
+def insert_custom_css(content):
+    body_end = next(re.finditer(r"</body>", content))
+    content = content[:body_end.start()] + """
+    <script type="text/javascript">
+        var linka = document.createElement("link");
+        linka.href = "media/cv-print.css";
+        linka.type = "text/css";
+        linka.rel = "stylesheet";
+        linka.media = "screen,print";
+        document.getElementsByTagName( "head" )[0].appendChild(linka);
 
-def generate_html_file(target_folder):
-    html_file = os.path.join(target_folder, "resume.html")
-    # disable pywebcopy logger
-    logging.disable(logging.CRITICAL)
-    save_website("http://localhost:6419", "/tmp", "resume")
-    static_files = os.path.join(target_folder, "__")
-    dir_util.copy_tree("/tmp/resume/localhost/__", static_files)
-
-    with open(html_file, 'w') as f:
-        with open("/tmp/resume/localhost/index.html", 'r') as tmpf:
-            content = tmpf.read()
-        content = remove_title(content)
-        f.write(content)
-
-    dir_util.remove_tree("/tmp/resume")
-    return html_file, static_files
-
+        var linkb = document.createElement("link");
+        linkb.href = "media/foundation-icons/foundation-icons.css";
+        linkb.type = "text/css";
+        linkb.rel = "stylesheet";
+        linkb.media = "screen,print";
+        document.getElementsByTagName( "head" )[0].appendChild(linkb);
+    </script>
+    """ + content[body_end.start():]
+    t_pa = re.compile(r"<h1.*?Bowen Zhi.*?h1>", re.DOTALL)
+    title_end = next(re.finditer(t_pa, content))
+    content = content[:title_end.end()] + """
+        <div id="contact-info">
+        <i class="fi-home" style="margin-left:1em"></i>
+        <a href="https://www.linkedin.com/in/bowen-zhi-878756112/" style="margin-left:0.5em"> LinkedIn</a>
+        <i class="fi-mail" style="margin-left:1em"></i>
+        <a href="zhi.b@husky.neu.edu" style="margin-left:0.5em">zhi.b@husky.neu.edu</a>
+        <i class="fi-social-github" style="margin-left:1em"></i>
+        <a href="https://github.com/zz920" style="margin-left:0.5em">zz920</a>
+        <i class="fi-telephone" style="margin-left:1em">+971504388108</i>
+        </div>
+    """ + content[title_end.end():]
+    return content
 
 def generate_pdf_file(html_file, pdf_file):
-    process = subprocess.Popen("wkhtmltopdf {} {}".format(html_file, pdf_file), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen("wkhtmltopdf -T 0 {} {}".format(html_file, pdf_file), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     process.communicate(timeout=3)
     process.terminate()
 
@@ -62,16 +79,12 @@ def process(source, target, format):
     if not os.path.isdir(target_folder):
         print("The output folder {} not exists, please input a valid folder!".format(target))
         return False
-
-    process = start_grip_server(source_file)
-    if not process:
-        return False
+    static_file = dir_util.copy_tree("./media", os.path.join(target_folder, "media"))
     try:
-        html_file, static_files = generate_html_file(target_folder)
-    except:
-        close_grip_server(process)
+        html_file = start_grip_server(source_file, target_folder)
+    except Exception as e:
+        print(e)
         return False
-    close_grip_server(process)
 
     if format == "html":
         print("The resume located in {}, please check".format(html_file))
@@ -82,9 +95,8 @@ def process(source, target, format):
 
     if not os.path.isfile(pdf_file):
         print("FAILED TO GENERATE THE PDF!")
-    dir_util.remove_tree(static_files)
-    os.remove(html_file)
-
+    # os.remove(html_file)
+    # os.remove(static_file)
     return True
 
 
--- resume_en.md
+++ resume_en.md
@@ -1,67 +1,36 @@
-# Info
+# Bowen Zhi
+## SKILL
 
- - Bowen Zhi/male/1992 
- - Master Of Computer Engineering/NEU,US
- - 2 years work experience
- - Github：http://github.com/zz920
-
----
-
-# CONTACT
-
-- Phone/Whatsup：+971504388108 
-- Email：zhi.b@husky.neu.edu
-- LinkedIn: https://www.linkedin.com/in/bowen-zhi-878756112/
-
----
-
-# SKILL
-
-- Language：Python/Java
+- Language：Python/Golang
 - Web Framework：Flask/Django/Sanic/SpringBoot
 - DataBase：MySQL/PgSQL/MongoDB/Redis
 - AWS Related：EC2/ECS/Elastic Beanstalk/S3/RDS
-- Tools：Git/Docker/FileBeat/Kibana/Elaticsearch
 
----
-
-# WORK EXPERIENCE
+## WORK EXPERIENCE
 
  
-## Fetchr,UAE （ 11/2017 ~ now ）
-Software Engineer
-### NPS（Airwaybill generation system）
-* Developed and maintained the PDF generation system based on Flask, generating more than 400 A4 PDF files per second during the peak.
+### Fetchr SDE,UAE ( 11/2017 ~ now ）
+#### NPS（Airwaybill generation system）
+* Developed and maintained the high performance PDF generation system based on Flask, generating more than 400 A4 PDF files per second during the peak.
 * Re-design the whole system with master-slave structure, seperating the CPU consumption part from the real-time requests, reduced more than 50% of response time. 
 * Migrate the whole system to Docker and deployed in the ECS, reducing 50% of the CPU time consumption and 60% budget of server. 
 * Rewrite the interface to WKHtmlToPDF lib, promise the zero-down time during the peak time. 
 
-### FMS(3rd party data syncing system)
+#### FMS(3rd party data syncing system)
 * Optimized the code logic, re-design the db models, and migrate the data from mongodb to mysql, reduce more than 50% query time and memory consumption.
-* Rewrite the syncing logic with 3rd Party delivery system. Enhance the reliability and maintainability of the server.
 * Dockerfy the system, migrate it from EC2 to ECS with auto-scalability. Optimized and migrated the log system to Elasticsearch/Kibana server.
 
 
-### IMS/CCDS(Invoice system)
-* Contributed to few APIs and Admin pages for finance team to handle the invoice generation,based on Django framework.
-* Release reliable based on Celery. 
+#### IMS/CCDS(Invoice system)
+* Contributed to few APIs and Admin pages for finance team to handle the invoice generation,based on Django framework. Release reliable based on Celery. 
 * Optimized PDF generation logic, reduced more 50% of generation time.
 
 
-### Other Server
-* Maintain few more servers, also contribute some Spring code to some Java system.
+### Part time Freelancer, Top Coder ( 02/2017 ~ 09/2017 )
+* Developed a Python CLT based on libclang to Parse the C code project, Traversing the call trees and Visualizing them into Functional Views.
+* Implemented the popular Page Rank algorithm to rank the function nodes and produced the output graph in HTML based on Bootstrap framework.
+* Won 2nd place (out of 38 registrations) and $600 prize for the project.
 
-## Part time Freelancer, Top Coder( 02/2017 ~ 09/2017 )
+## PERSONAL PROJECT
 
-
-## NIHON KHONDEN, CHINA （ 09/2014 ~ 05/2015 ）
-
-### ECG Team
-* Worked in a team responsible for design and maintenance of embedded operating system.
-* Improved older system transmission capability and reduce cardiogram storage memory cost by 60%
-
----
-
-# PERSONAL PROJECT
-
- - [SANIC_PDF](https://github.com/zz920/sanic_pdf)：The pdf server based on the async framework, converting html to pdf and uploading to s3 bucket.
+ - [SANIC_PDF](https://github.com/zz920/sanic_pdf)：Pdf generation server based on the async framework.
